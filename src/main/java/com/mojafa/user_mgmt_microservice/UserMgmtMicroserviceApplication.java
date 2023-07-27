59577189e1d94488b2ad521c11a6ea756417593d
mojafa <jmohsa@gmail.com>
2023-07-28 02:28:55 +0300

    initial commit

---
+++ src/main/java/com/mojafa/user_mgmt_microservice/UserMgmtMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mojafa.user_mgmt_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMgmtMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMgmtMicroserviceApplication.class, args);
+	}
+
+}
